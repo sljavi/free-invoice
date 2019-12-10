d993bcbc2be227b568f9570816a80e59356552cd
Javier Perez <javi.pzv@gmail.com>
2019-12-09 23:12:19 -0300

    Version build folder

---
+++ build/precache-manifest.8e8c83922f42a18d377072c2e10192cf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c1a5fe36013889c624b1f28fb88913",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dd827ab8510af159e3f",
+    "url": "/static/css/main.14e00b07.chunk.css"
+  },
+  {
+    "revision": "ecd1172c11aba40acf68",
+    "url": "/static/js/2.8ec02c83.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8ec02c83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9dd827ab8510af159e3f",
+    "url": "/static/js/main.db587029.chunk.js"
+  },
+  {
+    "revision": "210359c11f6d02eac15c",
+    "url": "/static/js/runtime-main.80cbac83.js"
+  },
+  {
+    "revision": "42497fb30dbdc61ccb8d59fa8c27a1f2",
+    "url": "/static/media/printer.42497fb3.svg"
+  },
+  {
+    "revision": "1731195fa1171e46db6c945db25dd722",
+    "url": "/static/media/save.1731195f.svg"
+  }
+]);
